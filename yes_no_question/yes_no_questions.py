d2690c3379ac6f8d93fca334822abd4d3ad117e0
huiwenz <huiwenz@andrew.cmu.edu>
2016-03-05 15:10:40 -0500

    Generating simple yes-no questions finished.
    Still need to identify name to decide whether the first letter need to
    be un-capitalized.

---
+++ yes_no_question/yes_no_questions.py
@@ -0,0 +1,47 @@
+# NOTE: This script takes a single sentence and generate a yes/no question based
+# on the sentence
+import nltk
+import sys
+import en
+
+# Parse the sentence
+sentence = sys.argv[1]
+
+tokens = nltk.word_tokenize(sentence)
+tagged = nltk.pos_tag(tokens)
+
+# For debugging
+print(tagged)
+
+# Go through the tags
+for i in xrange(len(tagged)):
+    te = tagged[i]
+    t = te[1]
+    if t == 'VBZ':
+        # 1. IS
+        if (te[0] == 'is'):
+            print('Is ' + ' '.join(tokens[:i]) + ' ' + ' '.join(tokens[i+1:len(tokens)-1]) + '?')
+        # 2. Does
+        else:
+            print('Does ' + ' '.join(tokens[:i]) + ' ' + en.verb.present(te[0]) + ' ' +
+                ' '.join(tokens[i+1:len(tokens)-1]) + '?')
+    # Past tense
+    elif t == 'VBD':
+        # Was
+        if (te[0] == 'was'):
+            print('Was ' + ' '.join(tokens[:i]) + ' ' + ' '.join(tokens[i+1:len(tokens)-1]) + '?')
+        else:
+            print('Did ' + ' '.join(tokens[:i]) + ' ' + en.verb.present(te[0]) + ' ' +
+                ' '.join(tokens[i+1:len(tokens)-1]) + '?')
+    # Modal
+    elif t == 'MD':
+        # Capitalize first letter
+        print(tokens[i].title() + ' ' + ' '.join(tokens[:i]) + ' ' + ' '.join(tokens[i+1:len(tokens)-1]) + '?')
+
+    # Present tense
+    elif t == 'VBP':
+        # Are
+        if (te[0] == 'are'):
+            print('Are ' + ' '.join(tokens[:i]) + ' ' + ' '.join(tokens[i+1:len(tokens) - 1]) + '?')
+        else:
+            print('Do ' + ' '.join(tokens[:len(tokens) - 1]) + '?')
