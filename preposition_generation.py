7c4a0984ab7f7c1817fffe587386aa92273f3299
JONASN24 <jonathanzeng24@gmail.com>
2016-03-16 17:01:46 -0400

    added dummy func

--- preposition_generation.py
+++ preposition_generation.py
@@ -1,6 +1,29 @@
 import nltk
 from nltk.parse.stanford import StanfordParser
 
+sentences = ["I talked to Sam.", 
+             "Professor came from a weird country.", 
+             "Sam went to the restroom at 11:30pm.",
+             "She will vote for Sam."]
+
+qkey_dict = dict()
+qkey_dict['PERSON'] = 'Who'
+qkey_dict['PLACE']  = 'Where'
+qkey_dict['TIME']   = 'When'
+qkey_dict['THING']  = 'What'
+qkey_dict['REASON'] = 'Why'
+qkey_dict['DEGREE'] = 'How'
+
+def get_type(word):
+    if word == 'Sam':
+        return 'PERSON'
+    elif word == 'restroom':
+        return 'PLACE'
+    elif word == 'country':
+        return 'PLACE'
+    else:
+        return "THING"
+
 def find_next_noun(tag_list,prop_ind):
     choices = tag_list[prop_ind+1:]
     length = len(choices)
@@ -26,37 +49,36 @@ def find_verb(tag_list,prop_ind):
         if choices[ind][:2] == 'VB':
             return length-ind-1
 
-sentences = ["I talked to Sam.", 
-             "Professor came from a weird country.", 
-             "Sam went to the restroom at 11:30pm.", 
-             "I like pictures of Sam.",
-             "She will vote for Sanders."]
-
 #identify "to" and other proposition first.
 #find 
 for sentence in sentences:
     tokens = nltk.word_tokenize(sentence)
     tagged = nltk.pos_tag(tokens)
-    
+    print tagged
+    # entities = nltk.chunk.ne_chunk(tagged)
 
-    #some processing
+    # some processing
     tag_list = [t for (w,t) in tagged]
     try:
         prop_ind = tag_list.index('TO')
     except:
         prop_ind = tag_list.index('IN')
+
     prop_word = tagged[prop_ind][0]
     #Where, who, how, why, what
     prev_noun_ind = find_prev_noun(tag_list,prop_ind)
     prev_noun = tagged[prev_noun_ind][0]
+    prev_type = get_type(prev_noun)
+
 
     next_noun_ind = find_next_noun(tag_list,prop_ind)
     next_noun = tagged[next_noun_ind][0]
+    next_type = get_type(next_noun)
 
     verb_ind = find_verb(tag_list,prop_ind)
     verb = tagged[verb_ind][0]
 
-    print sentence
+    # print sentence
     # print prev_noun
     # print next_noun
     # print verb
@@ -64,12 +86,14 @@ for sentence in sentences:
     verb_property = tag_list[verb_ind]
     if verb_property == 'VBD':
         do_word = 'did'
+        verb = 
     elif verb_property == 'VBP':
         do_word = 'does'
     else:
         do_word = 'do'
 
-    print ' '.join(['What',do_word,prev_noun,verb,tagged[prop_ind][0], '?'])
+    start_key = qkey_dict[next_type]
+    print ' '.join([start_key,do_word,prev_noun,verb,tagged[prop_ind][0], '?'])
     print '\n'
 
     
